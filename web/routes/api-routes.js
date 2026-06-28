5fc9e22dead2404594a046e0f92c84a1acaaa511
ankitR3 <ankitraj828401@gmail.com>
2026-06-28 23:34:10 +0530

    fix api routes and connect backend

--- web/routes/api-routes.js
+++ web/routes/api-routes.js
@@ -3,7 +3,7 @@ const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 export const API_URL = BACKEND_URL + '/api/v1/task';
 
 export const CREATE_TASK = API_URL + '/';
-export const UPDATE_TASK = API_URL + '/:id';
-export const DELETE_TASK = API_URL + '/:id';
 export const GET_ALL_TASK = API_URL + '/';
-export const UPDATE_TASK_STATUS = API_URL + '/:id/status';
+export const UPDATE_TASK = (id) => `${API_URL}/${id}`;
+export const DELETE_TASK = (id) => `${API_URL}/${id}`;
+export const UPDATE_TASK_STATUS = (id) => `${API_URL}/${id}/status`;
